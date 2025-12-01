4d53f4937c847cd2c78a7eb7d7d159f9f01f4b81
netrasb <netrabijapur02@gmail.com>
2025-12-01 23:42:55 +0530

    Initial commit

---
+++ Basics/Solution.java
@@ -0,0 +1,6 @@
+package Basics;
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("ALL THE  BEST "); // Prints the message to standard output
+    }
+}
