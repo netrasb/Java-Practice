7b271f1e01af1ad85e878ae513c13fc6e1591e36
netrasb <netrabijapur02@gmail.com>
2025-12-05 23:20:03 +0530

    Daily practice

---
+++ Geeks.java
@@ -0,0 +1,36 @@
+// Java program to reverse a number
+// and find if it is a palindrome or not
+
+// Driver Class
+class Geeks
+{
+    // Recursive function to reverse
+    // the digits of number
+    static int recursive_func(int n, int rev)
+    {
+        if (n < 10) {
+            return rev * 10 + n;
+        }
+        else {
+            int last_digit = n % 10;
+            rev = rev * 10 + last_digit;
+            return recursive_func(n / 10, rev);
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int n = 123464321;
+        int rev = recursive_func(n, 0);
+
+        System.out.println("Reverse of n = " + rev);
+
+        // Checking if n is same
+        // as reverse of n
+        if (n == rev)
+            System.out.println("Palindrome = Yes");
+        else
+            System.out.println("Palindrome = No");
+    }
+}
