fd902198750b5e568ace952ce24ee8279c91ad4e
netrasb <netrabijapur02@gmail.com>
2025-12-07 23:12:33 +0530

    Daily practice

---
+++ PrimeCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (n < 2) isPrime = false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+}
