d26b5b3aea11e42fe86deb4ad8f8ecf0fb898693
netrasb <netrabijapur02@gmail.com>
2025-12-10 22:15:39 +0530

    Daily practice

---
+++ FibonacciSeries.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many Fibonacci numbers to print: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+        System.out.print(a + " " + b + " ");
+
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
